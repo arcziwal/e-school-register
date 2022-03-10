4be256a2b00ded6b24fa3fcb58cc7981ed32e67a
Artur Waligora <waligoraartur95@gmail.com>
2022-03-10 20:54:11 +0100

    Parent and Student models added

--- register_app/models.py
+++ register_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Parent(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=64)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=32)
+    second_name = models.CharField(max_length=32, null=True)
+    last_name = models.CharField(max_length=64)
+    birth_date = models.DateField()
+    create_date = models.DateField(auto_now_add=True)
+    pesel = models.CharField(max_length=11)
+    address = models.CharField(max_length=32, null=True)
+    city = models.CharField(max_length=32, null=True)
+    zip_code = models.CharField(max_length=6, null=True)
+    parents = models.ManyToManyField(Parent)
+    school_class = models.ForeignKey('SchoolClass', on_delete=models.SET_NULL, null=True)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+
+class SchoolClass(models.Model):
+    pass
